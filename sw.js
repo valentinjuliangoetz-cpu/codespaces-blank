f57b5edf64c8b9e5074926cff0abcabc60bf2db7
valentinjuliangoetz-cpu <valentinjuliangoetz@gmail.com>
2025-10-26 16:36:30 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='ascendant-cache-v6-3';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
